50181e46eb289136a9d0c1c5904ae62085baf977
Amine-Triki <amine.triki15@gmail.com>
2024-07-28 14:11:30 +0100

    adding pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,26 @@
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants.js";
+
+
 /** @type {import('next').NextConfig} */
+
+
+
 const nextConfig = {
-    images: {
-      domains: [],
-    },
-  };
-export default nextConfig;
+  reactStrictMode: true,
+};
+
+
+const nextConfigFunction = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import("@ducanh2912/next-pwa")).default({
+      dest: "public",
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};
+
+export default nextConfigFunction;
